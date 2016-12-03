b65820a9f63ff43fc1cad56ca3f0fee15aa29986
Nguyen Hung My <nguyenhungmy22@gmail.com>
2016-12-03 21:18:09 +0700

    First code

---
+++ Listen English/Listen English/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Listen English
+//
+//  Created by Nguyễn Hùng Mỹ on 12/3/16.
+//  Copyright (c) 2016 Nguyễn Hùng Mỹ. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+ 
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+      
+    }
+}
